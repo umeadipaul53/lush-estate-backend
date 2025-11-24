c8e00722e869e96d1b14f68576fc903ff9d84d19
umeadipaul53 <umeadipaul53@gmail.com>
2025-11-24 15:51:18 +0100

    fix

--- controller/webhook/signup.js
+++ controller/webhook/signup.js
@@ -8,31 +8,31 @@ const signup = async (req, res, next) => {
 
     console.log("SYTEMAP Signup Webhook:", payload);
 
-    const name = `${payload.Firstname} ${payload.Lastname}`;
-    const email = payload.Email;
+    // const name = `${payload.Firstname} ${payload.Lastname}`;
+    // const email = payload.Email;
 
-    // Check if user already exists
-    let user = await userModel.findOne({ email });
+    // // Check if user already exists
+    // let user = await userModel.findOne({ email });
 
-    if (!user) {
-      // Create new user
-      user = await userModel.create({
-        sytemapUserId: payload.BaseUserId,
-        name,
-        phone: payload.Mobile,
-        email,
-        currentStep: 1,
-      });
-    } else {
-      // If user exists but sytemapUserId missing → update it
-      if (!user.sytemapUserId) {
-        user.sytemapUserId = payload.BaseUserId;
-        await user.save();
-      }
-    }
+    // if (!user) {
+    //   // Create new user
+    //   user = await userModel.create({
+    //     sytemapUserId: payload.BaseUserId,
+    //     name,
+    //     phone: payload.Mobile,
+    //     email,
+    //     currentStep: 1,
+    //   });
+    // } else {
+    //   // If user exists but sytemapUserId missing → update it
+    //   if (!user.sytemapUserId) {
+    //     user.sytemapUserId = payload.BaseUserId;
+    //     await user.save();
+    //   }
+    // }
 
     // Always respond immediately with 200 so SYTEMAP considers webhook successful
-    return res.status(200).json({ status: "ok" });
+    return res.status(200).json({ status: "ok", data: payload });
   } catch (error) {
     console.error("SYTEMAP Signup Webhook Error:", error);
     return next(new AppError("Internal Server Error", 500));
